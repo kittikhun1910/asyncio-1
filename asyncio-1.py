96d6241cc676ec039c961e1797accb29cfeef5ad
Kittikhun <tumgamemar@gmail.com>
2023-07-12 14:03:36 +0700

    initail commit

---
+++ asyncio-1.py
@@ -0,0 +1,20 @@
+# example of runing a coroutine
+import asyncio
+
+# define a coroutine
+
+
+async def custom_coro():
+    # wait for a take to be done
+    # a wait another coroutine
+    await asyncio.sleep(1)
+
+# main coroutine
+
+
+async def main():
+    # execute my custom coroutine
+    await custom_coro()
+
+# start the corotine programs
+asyncio.run(main())
